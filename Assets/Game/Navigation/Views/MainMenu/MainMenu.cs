06196e948a031cd8d99481102d86b21ec90d3eb5
Michael-Rotteveel <michael_rotteveel@hotmail.nl>
2025-11-10 17:14:50 +0100

    [TASK] AudioSystem refactor + Debug Console + Localization

--- Assets/Game/Navigation/Views/MainMenu/MainMenu.cs
+++ Assets/Game/Navigation/Views/MainMenu/MainMenu.cs
@@ -1,21 +1,27 @@
 ﻿using Core.Systems.AudioSystem;
+using Core.Systems.Localization.Definitions;
 using Core.Systems.Navigation;
 using Core.Systems.PopUp;
+using Core.Utility.Attributes;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Game.Navigation.Views.MainMenu{
     public class MainMenu : MonoBehaviour
     {
+        [Inject] private AudioService _audioService;
         [SerializeField] private SoundData themeMusic;
         [SerializeField] private UIDocument document;
+        [Header("Localization")]
+        [SerializeField] private LocalizedString startButtonText;
+        
         private Button startButton;
         public void OnOpen(){
             
             
             
             document.rootVisualElement.Q("container").style.opacity = 1f;
-            SoundManager.Instance.CreateSound()
+            _audioService.CreateSound()
                 .WithSoundData(themeMusic)
                 .Play();
             
@@ -24,6 +30,7 @@ namespace Game.Navigation.Views.MainMenu{
 
         private void InitMenu(VisualElement root){
             startButton = root.Q<Button>("StartButton");
+            startButton.text = startButtonText.GetLocalizedValue();
             startButton.clicked += PopupRegistrar.TestPopups;
         }
 
